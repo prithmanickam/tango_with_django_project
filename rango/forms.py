ac0ff516ef804710ca90e6ba9879b7e5d6efa0eb
Prith Manickam <2518533m@student.gla.ac.uk>
2022-02-11 00:48:41 +0000

    Ch9 completed, tests not working

--- rango/forms.py
+++ rango/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,help_text="Please enter the category name.")
-    
-    
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -34,3 +34,14 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
         
         return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
